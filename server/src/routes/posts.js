5b1c5c487826d8e0228f7c1b38a32f522ebbac47
noz220399@gmail.com <36004990+IllyNozdrevatih@users.noreply.github.com>
2020-04-28 00:14:07 +0300

    Update posts.js

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const postController = require('../models/post')
+const postController = require('../controllers/post')
 
 router.post('/', postController.getIndex)
 
