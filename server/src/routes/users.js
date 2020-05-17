6e6f457b9040d04ece2049d8165576de92a0b0fb
DenisKornienko1337 <64380485+DenisKornienko1337@users.noreply.github.com>
2020-05-18 00:11:03 +0300

    auth

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/users')
+const authController = require('../controllers/auth')
+
 const isAuth = require('../middleware/is-auth')
 const isSuperuser = require('../middleware/is-superuser')
 
 router.post('/add-user', usersController.addUser)
 router.post('/login', usersController.logIn)
 router.get('/logout', usersController.logOut)
+router.get('/is-auth', authController.isAuth)
 router.get('/permissions', usersController.fetchPermissions)
 router.post('/delete-user', isAuth, isSuperuser, usersController.removeUser);
 
