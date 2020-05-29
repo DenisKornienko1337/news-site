2c2edee60c1c7d26590032e187db30c505937aa7
DenisKornienko1337 <64380485+DenisKornienko1337@users.noreply.github.com>
2020-05-29 23:25:48 +0300

    username

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -2,6 +2,6 @@ exports.isAuth = (req, res) => {
     res.send({
         isLoggedIn: req.session.isLoggedIn,
         user: req.session.user,
-        user: req.session.username,
+        username: req.session.username,
     })
 }
