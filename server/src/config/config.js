5269895b47ab379983a7d59203a98617ad6d7b36
Илья Ноздреватых <i.nozdrevatih@gmail.com>
2020-06-28 15:36:11 +0300

    update server

--- server/src/config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    clientHost: 'http://mevn.cr59594.tmweb.ru/',
-    port: 8081,
-    dbURL: 'mongodb+srv://den:test1337@cluster0-lwu80.mongodb.net/blog',
-    //dbOptions: { useMongoClient: true},
-  }
